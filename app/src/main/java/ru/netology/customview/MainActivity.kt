2f57d161b234fa7f5f44e5df3b847617f564cffa
ERokhmanko <rizhii_mishonok@mail.ru>
2022-01-18 16:45:38 +0300

    code from the webinar

---
+++ app/src/main/java/ru/netology/customview/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.netology.customview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.netology.customview.ui.StatsView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<StatsView>(R.id.view_circle).data = listOf(
+            0.25F,
+            0.25F,
+            0.5F
+        )
+    }
+}
